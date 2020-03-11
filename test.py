7675bdfddcea81d4066d6aed71f27568b54a8b72
David Guedalia <idavidg@gmail.com>
2020-03-11 11:10:49 +0200

    better

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 #
 import pandas as pd
-df = pd.read_json('NECSI-TRAVELDATAVIZ-20200309-1846.json').T
+df = pd.read_json('NECSI-TRAVELDATAVIZ-20200309-1846.old.json').T
 df['DATE']=pd.to_datetime(df.DATE, format='%d.%m.%Y', errors='coerce')
 df['day_of_year'] = df['DATE'].dt.dayofyear + (df['DATE'].dt.year - 2019) * 365
 df['day_of_year'] = df['day_of_year'] - df['day_of_year'].min()
-df = df.sort_values('day_of_year').dropna()
+df = df.sort_values('day_of_year').reset_index(drop=True).dropna()
 
-df.to_json('NECSI-TRAVELDATAVIZ-20200309-1846.v1.json', orient='index')
+df.to_json('NECSI-TRAVELDATAVIZ-20200309-1846.json', orient='index')
 
 
